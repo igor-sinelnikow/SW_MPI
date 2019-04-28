54770c214f13d613151f130513388953b63879ff
Синельников Игорь <igor.s@polus-ib.bmc.hpc.cs.msu.ru>
2019-04-28 11:34:03 +0300

    Scripts from IBM Polus [2]

--- concat_polus.sh
+++ concat_polus.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
+dir=results
+
 # len1=67106
 # len2=64000
 len1=64113
 len2=62000
 
-dir=results
-
 exec=simmtx_omp
-for p in 1 2 4 8 10 20; do
-    for (( t = 1; t <= 8; t *= 2 )); do
-        tail -n 1 -q ${dir}/${exec}.${len1}x${len2}-p${p}-t${t}.*.txt >> ${dir}/${exec}.${len1}x${len2}-p${p}-t${t}.txt
-    done
+for p in 1 2 4 10; do
+    let "t=160/$p"
+    rm -f ${dir}/${exec}.${len1}x${len2}-p${p}-t${t}.txt
+    tail -n 1 -q ${dir}/${exec}.${len1}x${len2}-p${p}-t${t}.*.txt >> ${dir}/${exec}.${len1}x${len2}-p${p}-t${t}.txt
 done
 
-exec=simmtx_gpu
-for dev in 1 2; do
-    p=${dev}
-    tail -n 1 -q ${dir}/${exec}.${len1}x${len2}-p${p}-dev${dev}.*.txt >> ${dir}/${exec}.${len1}x${len2}-p${p}-dev${dev}.txt
-done
+# exec=simmtx_gpu
+# for dev in 1 2; do
+#     p=${dev}
+#     tail -n 1 -q ${dir}/${exec}.${len1}x${len2}-p${p}-dev${dev}.*.txt >> ${dir}/${exec}.${len1}x${len2}-p${p}-dev${dev}.txt
+# done
--- run_polus.sh
+++ run_polus.sh
@@ -3,46 +3,33 @@
 limit=1
 task_dist=pack
 queue="-q normal"
-# len1=67106
-# len2=64000
-len1=64113
-len2=62000
 
 mkdir -p results
 
-for (( i = 1; i <= 3; i++ )); do
-    exec=simmtx_omp
-    for p in 1 2 4 8 10 20; do
-        for (( t = 8; t >= 1; t /= 2 )); do
-            l=`bjobs 2> /dev/null | wc -l`
-            while (( l-1 >= limit )); do
-                sleep 7
-                l=`bjobs 2> /dev/null | wc -l`
-            done
-            bsub -n ${p} -a "p8aff(${t},${t},${t},${task_dist})" -R "select[(maxmem==256G) && (type==any)] same[nthreads]" -env "all, OMP_DISPLAY_ENV=FALSE, OMP_DYNAMIC=FALSE, OMP_SCHEDULE=STATIC" ${queue} -e results/${exec}.${len1}x${len2}-p${p}-t${t}.%J.txt -J "SW_MPI" mpiexec ./${exec} ../data/${len1}.target ${len1} ../data/${len2}.query ${len2} 2 -1 -2
-        done
-    done
-
-    exec=simmtx_gpu
-    for dev in 1 2; do
+len1=67106
+len2=64000
+# len1=64113
+# len2=62000
+exec=simmtx_omp
+for (( i = 1; i <= 1; i++ )); do
+    for p in 1 2; do
+        let "t=160/$p"
         l=`bjobs 2> /dev/null | wc -l`
         while (( l-1 >= limit )); do
             sleep 7
             l=`bjobs 2> /dev/null | wc -l`
         done
-        p=${dev}
-        bsub -n ${p} -gpu "num=${dev}:mode=exclusive_process" -R "select[(maxmem==256G) && (type==any)]" ${queue} -e results/${exec}.${len1}x${len2}-p${p}-dev${dev}.%J.txt -J "SW_GPU" mpiexec ./${exec} ../data/${len1}.target ${len1} ../data/${len2}.query ${len2} 2 -1 -2
+        bsub -n ${p} -a "p8aff(${t},8,8,${task_dist})" -R "select[(maxmem==256G) && (type==any)] same[nthreads]" -env "all, OMP_DISPLAY_ENV=FALSE, OMP_DYNAMIC=FALSE, OMP_SCHEDULE=STATIC" ${queue} -e results/${exec}.${len1}x${len2}-p${p}-t${t}.%J.txt -J "SW_MPI" mpiexec ./${exec} ../data/${len1}.target ${len1} ../data/${len2}.query ${len2} 2 -1 -2
     done
-done
 
-exec=simmtx_gpu
-dev=2
-p=${dev}
-for (( i = 4; i <= 10; i++ )); do
-    l=`bjobs 2> /dev/null | wc -l`
-    while (( l-1 >= limit )); do
-        sleep 7
-        l=`bjobs 2> /dev/null | wc -l`
-    done
-    bsub -n ${p} -gpu "num=${dev}:mode=exclusive_process" -R "select[(maxmem==256G) && (type==any)]" ${queue} -e results/${exec}.${len1}x${len2}-p${p}-dev${dev}.%J.txt -J "SW_GPU" mpiexec ./${exec} ../data/${len1}.target ${len1} ../data/${len2}.query ${len2} 2 -1 -2
+    # exec=simmtx_gpu
+    # for dev in 1 2; do
+    #     l=`bjobs 2> /dev/null | wc -l`
+    #     while (( l-1 >= limit )); do
+    #         sleep 7
+    #         l=`bjobs 2> /dev/null | wc -l`
+    #     done
+    #     p=${dev}
+    #     bsub -n ${p} -gpu "num=${dev}:mode=exclusive_process" -R "select[(maxmem==256G) && (type==any)]" ${queue} -e results/${exec}.${len1}x${len2}-p${p}-dev${dev}.%J.txt -J "SW_GPU" mpiexec ./${exec} ../data/${len1}.target ${len1} ../data/${len2}.query ${len2} 2 -1 -2
+    # done
 done
